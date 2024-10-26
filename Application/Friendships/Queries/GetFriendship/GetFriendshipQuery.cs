4e63c1c73f4f709c77807e33b278893b799d2f26
Cezar <peleacezar@icloud.com>
2024-10-26 10:52:31 +0300

    add friendship requests

---
+++ Application/Friendships/Queries/GetFriendship/GetFriendshipQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Core.Abstractions.Messaging;
+using Contracts.Friendships;
+using Domain.Core.Primitives.Maybe;
+
+namespace Application.Friendships.Queries.GetFriendship;
+
+/// <summary>
+/// Represents the query for getting the friendship for the user and friend identifiers.
+/// </summary>
+public sealed class GetFriendshipQuery : IQuery<Maybe<FriendshipResponse>>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetFriendshipQuery"/> class.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="friendId">The friend identifier.</param>
+    public GetFriendshipQuery(Guid userId, Guid friendId)
+    {
+        UserId = userId;
+        FriendId = friendId;
+    }
+
+    /// <summary>
+    /// Gets the user identifier.
+    /// </summary>
+    public Guid UserId { get; }
+
+    /// <summary>
+    /// Gets the friend identifier.
+    /// </summary>
+    public Guid FriendId { get; }
+}
